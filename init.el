243f7548ffd910bf4bf246c537091f6d858f25fe
Kevin Alemi <kevin.alemi97@gmail.com>
2017-10-25 11:00:39 +0200

    Added init file

---
+++ init.el
@@ -0,0 +1,340 @@
+
+;; This is the official Emacs init file for the course IOOPM. It
+;; uses English, not because anyone thinks it's cooler than
+;; Swedish, but to avoid encoding problems with using Swedish
+;; characters. Put it in `~/.emacs.d/init.el` to use it for your
+;; own Emacs. You can use as many or as few of these settings as
+;; you would like. Experiment and try to find a set-up that suits
+;; you!
+;;
+;; Some settings in this file are commented out. They are the ones
+;; that require some choice of parameter (such as a color) or that
+;; might be considered more intrusive than other settings (such as
+;; linum-mode).
+;;
+;; Whenever you come across something that looks like this
+;;
+;;    (global-set-key (kbd "C-e") 'move-end-of-line)
+;;
+;; it is a command that sets the keyboard shortcut for some
+;; function. If you find a function that you like, whose keyboard
+;; shortcut you don't like, you can (and should!) always change it to
+;; something that you do like.
+
+;; ===========
+;; Appearance
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+
+(load-theme 'wombat)
+;;(set-face-attribute 'default nil :height 150)
+
+
+(global-linum-mode t)
+
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+
+(add-hook 'c-mode-hook (lambda () (font-lock-add-keywords nil '(("\\<\\(malloc\\|calloc\\|free\\|realloc\\)\\>" . font-lock-warning-face)))))
+
+
+;; Disable the menu bar
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+
+;; Disable the tool bar
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+
+;; Disable the scroll bar
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+;; Turn off annoying splash screen
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-echo-area-message t)
+(setq inhibit-startup-message t)
+
+;; When in doubt, use text-mode
+(setq default-major-mode 'text-mode)
+
+;; Show time using Swedish format
+(setq display-time-day-and-date t
+      display-time-24hr-format t)
+(display-time)
+
+
+;; Set which colors to use
+;; You can see a list of all the available colors by checking the
+;; variable "color-name-rgb-alist" (Type "C-h v color-name-rgb-alist
+;; <RET>"). Most normal color names work, like black, white, red,
+;; green, blue, etc.
+;(set-background-color "black")
+;(set-foreground-color "white")
+(set-cursor-color "white")
+
+;; Set a custom color theme
+;; You can also try using a custom theme, which changes more colors
+;; than just the three above. For a list of all available themes,
+;; press "M-x customize-themes <RET>". You can also use a theme in
+;; combination with the above set-color-commands.
+
+;; =========
+;; Packages
+;; =========
+
+;; Use more package-archives (M-x list-packages)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; Tell Emacs where to look for packages
+(let ((default-directory "~/.emacs.d/ioopm-packages/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(let ((default-directory "~/.emacs.d/elpa/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+
+
+;; Show both line and column number in the bottom of the buffer
+(column-number-mode t)
+
+;; Show parenthesis matching the one below the cursor
+(show-paren-mode t)
+
+;; Show line numbers to the left of all buffers
+; (global-linum-mode t)
+
+;; Sentences are not followed by two spaces
+;; Makes navigating with M-e and M-a (forward/backward senctence)
+;; behave like you would expect
+(setq sentence-end-double-space nil)
+
+;; C-SPC after C-u C-SPC cycles mark stack
+(setq-default set-mark-command-repeat-pop t)
+
+;; Move entire paragraph with M-n/M-p
+(global-set-key "\M-n" 'forward-paragraph)
+(global-set-key "\M-p" 'backward-paragraph)
+
+;; Back to indentation with M-a
+(global-set-key "\M-a" 'back-to-indentation)
+
+;; Avy mode
+;; Jump anywhere on screen in four keystrokes or less.
+(require 'avy)
+(define-key global-map (kbd "C-j") 'avy-goto-word-or-subword-1)
+(define-key global-map (kbd "C-M-j") 'avy-goto-char)
+(define-key global-map (kbd "C-M-l") 'avy-goto-line)
+
+;; Ace window
+;; Select which window to switch to, rather than shuffling through them all
+(require 'ace-window)
+;(global-set-key (kbd "C-x o") 'ace-window)
+
+
+;; imenu
+;; Language-aware navigation
+(require 'imenu-anywhere)
+(setq imenu-auto-rescan t)
+(global-set-key (kbd "C-.") 'imenu-anywhere)
+
+;; ===========
+;; Editing
+;; ===========
+
+;; Allow deletion of selected text with <DEL>
+(delete-selection-mode 1)
+
+;; Use multiple spaces instead of tab characters
+(setq-default indent-tabs-mode nil)
+
+;; hippie-expand instead of dabbrev-expand
+;; dabbrev-expand will try to expand the word under the cursor by
+;; searching your open buffers for words beginning with the same
+;; characters. For example, if you have written "printf" in an open
+;; buffer you can just write "pr" and expand it to the full
+;; word. hippie-expand does the same kind of search, plus some
+;; additional searching, such as in your kill ring or the names of the
+;; files you have open.
+(global-set-key [(control tab)] 'hippie-expand)
+
+;; Expand from everything!
+(setq hippie-expand-try-functions-list
+  '(try-expand-dabbrev
+    try-expand-dabbrev-all-buffers
+    try-complete-file-name-partially
+    try-complete-file-name
+    try-expand-all-abbrevs
+    try-expand-list
+    try-expand-dabbrev-from-kill
+    try-expand-line
+    try-complete-lisp-symbol-partially
+    try-complete-lisp-symbol))
+
+;;Auto complete mode
+;; Always suggest completions.
+ (require 'popup)
+ (require 'auto-complete)
+ (global-set-key (kbd "C-<return>")
+                 (lambda () (interactive)
+                   (progn (auto-complete-mode 1) (auto-complete))))
+
+;; Visual Regexp
+;; Replace normal query-replace with a better one
+(require 'visual-regexp)
+(global-set-key "\M-%" 'vr/query-replace)
+
+;; Multiple cursors
+;; What it sounds like
+(require 'multiple-cursors)
+
+;; Suggested bindings
+; (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+; (global-set-key (kbd "C-M-c") 'mc/edit-lines)
+
+;; Expand region
+;; Select the thing I'm currently inside
+(require 'expand-region)
+(global-set-key (kbd "M-h") 'er/expand-region)
+
+;; ==========
+;; Interface
+;; ==========
+
+;; Use alt for meta and cmd for super (interferes less with the OS)
+(setq mac-command-modifier  'super)
+(setq mac-option-modifier  'meta)
+
+;; Teach Emacs Swedish keyboard layout
+(define-key key-translation-map (kbd "M-8") (kbd "["))
+(define-key key-translation-map (kbd "M-9") (kbd "]"))
+
+(define-key key-translation-map (kbd "M-(") (kbd "{"))
+(define-key key-translation-map (kbd "M-)") (kbd "}"))
+
+(define-key key-translation-map (kbd "M-7") (kbd "|"))
+(define-key key-translation-map (kbd "M-/") (kbd "\\"))
+
+(define-key key-translation-map (kbd "M-2") (kbd "@"))
+(define-key key-translation-map (kbd "M-4") (kbd "$"))
+
+;; fn --> hyper
+;; Allow using fn as modifier
+; (setq mac-function-modifier 'hyper)
+
+;; ido mode
+;; Automatic auto-complete for many things, including opening files.
+(ido-mode)
+
+;; Don't automatically select things though...
+(setq ido-auto-merge-delay-time 9999)
+
+;; smex
+;; M-x on steroids
+(require 'smex)
+(global-set-key "\M-x" 'smex)
+
+;; Uniquify buffernames
+;; Give better names to buffers of same name
+(require 'uniquify)
+
+;; Save-place
+;; Remember the cursor position when you close a file, so that you
+;; start with the cursor in the same position when opening it again
+(setq save-place-file "~/.emacs.d/saveplace")
+(setq-default save-place t)
+(require 'saveplace)
+
+;; Recent files
+;; Enable a command to list your most recently edited files. If you
+;; know you are opening a file that you have edited recently, this
+;; should be faster than using find-file ("C-x C-f"). The code below
+;; binds this to the keyboard shortcut "C-x C-r", which replaces the
+;; shortcut for the command find-file-read-only.
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key "\C-x\C-r" 'recentf-ido-find-file)
+
+
+;; ============
+;; Programming
+;; ============
+
+;; Flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+
+
+
+;; Key binding for finding next error
+(dolist (hook '(prog-mode-hook))
+  (add-hook hook
+            (lambda () (interactive)
+              (local-set-key (kbd "C-c C-n")
+                             'next-error))))
+
+;; Key binding for finding previous error
+(dolist (hook '(prog-mode-hook))
+  (add-hook hook
+            (lambda () (interactive)
+              (local-set-key (kbd "C-c C-p")
+                             'previous-error))))
+
+;; Display flymake errors in minibuffer
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(e-selected-packages (quote (company-c-headers company)))
+ '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
+ '(help-at-pt-timer-delay 0.2)
+ '(package-selected-packages
+   (quote
+    (haskell-tab-indent haskell-mode flymake-haskell-multi company-c-headers auto-complete))))
+
+
+;; YASnippets
+;; Expand e.g. "for<tab>" to "for(int i = 0; i < N; i++) {}"
+(require 'yasnippet)
+(yas-global-mode 1)
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray0" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Monaco"))))
+ '(avy-background-face ((t (:background "black" :foreground "black"))))
+ '(aw-background-face ((t (:background "black" :foreground "gray1"))))
+ '(font-lock-warning-face ((t (:foreground "red2")))))
+(set-face-attribute 'default nil :family "Monaco" :height 147 :weight 'normal)
+
